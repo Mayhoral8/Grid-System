df2a73fdb60a4392f3164673c4b39743ebfdd984
Mayhoral8 <85770878+Mayhoral8@users.noreply.github.com>
2021-07-20 16:46:11 +0100

    Add files via upload

---
+++ FW.js
@@ -0,0 +1,10 @@
+
+let openFunction = function () {
+    let closeBtn = document.getElementById("menu");
+    closeBtn.style.width = "200px";
+    
+    }
+    let closeFunction = function () {
+        let closeBtn = document.getElementById("menu");
+        closeBtn.style.width = "0px";
+        }
